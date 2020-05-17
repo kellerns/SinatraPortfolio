f21bd2cc9e6e7e72c6b48fdc68944ce4ce1d1b1c
Nicholas Keller <nic.keller328@gmail.com>
2020-05-17 22:06:52 +0000

    erb file updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,15 @@ class UsersController < Sinatra::Base
     erb :create_user
   end
 
+  post '/users' do
+    @user = User.new(params[:user])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect '/reviews'
+    else
+      @error = "username or password invalid"
+      erb :create_user
+    end
+  end
 
 end
