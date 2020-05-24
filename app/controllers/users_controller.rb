09c515eb5c5c0bc21e83e5cf9d9713ba8bfe2de2
Nicholas Keller <nic.keller328@gmail.com>
2020-05-24 20:08:20 +0000

    Somehow my user controller and model werent getting committed but they should be good now.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,16 @@
 class UsersController < ApplicationController
-  get "/" do
-    erb :index
+
+  get "/signup" do
+    erb :'/users/new'
   end
 
-  #
-  # get "/signup" do
-  #   erb :signup
-  # end
-  #
-  # post "/signup" do
-  #   if params[:username]=="" || params[:password]==""
-  #     redirect to '/failure'
-  #   end
-  #
-  #   user = User.new(:username => params[:username], :password => params[:password])
-  #   if user.save
-	# 		redirect to "/login"
-	# 	else
-	# 		redirect to "/failure"
-	# 	end
-  # end
-  #
-  # get '/account' do
-  #   @user = User.find(session[:user_id])
-  #   erb :account
-  # end
-
+  post "/signup" do
+    user = User.new(params)
+    if user.save
+      redirect "/teams"
+    else
+      redirect "/signup"
+    end
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,5 @@
 class User < ActiveRecord::Base
   has_many :teams
   has_secure_password
-
-#   def slug
-#     self.username.downcase.gsub(" ", "-")
-#   end
-#
-#   def self.find_by_slug(slug)
-#     User.all.find do |user|
-#       if user.slug == slug
-#           user
-#       end
-#     end
-#   end
+  validates :name, :email, :password, presence: true
 end
