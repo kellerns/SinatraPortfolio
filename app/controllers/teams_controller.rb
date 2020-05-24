ea023e27bbeaa1109d78e2f1a3473931c7bd9a1c
Nicholas Keller <nic.keller328@gmail.com>
2020-05-24 15:58:23 +0000

    made the edit erb and updated the controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -13,7 +13,43 @@ class TeamsController < ApplicationController
   end
 
   post '/teams' do
+    user = User.find_by_id(params[:user_id])
+    team = user.teams.build(params)
+    if team.save
+      redirect '/teams'
+    else
+      redirect '/teams/new'
+    end
+  end
 
+  get '/teams/:id/edit' do
+    @users = User.all
+    @team = Team.find_by_id(params[:id])
+    #render the edit view
+    erb :"teams/edit"
+  end
+
+  patch '/teams/:id' do
+    @team = Team.find_by_id(params[:id])
+    if @team.update(
+      name: params[:name],
+      league: params[:league],
+      quarterback: params[:quarterback],
+      running_back_one: params[:running_back_one],
+      running_back_two: params[:running_back_two],
+      wide_receiver_one: params[:wide_receiver_one],
+      wide_receiver_two: params[:wide_receiver_two],
+      tight_end: params[:tight_end],
+      kicker: params[:kicker],
+      defense: params[:defense],
+      wins: params[:wins],
+      losses: params[:losses])
+      #redirect to show view
+      redirect "/teams/#{@team.id}"
+    else
+      #redirect to edit view
+      redirect "/teams/#{@team.id}/edit"
+    end
   end
 
   get'/teams/:id' do
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,4 @@
 class Team < ActiveRecord::Base
   belongs_to :user
+  validates :name, :league, :quarterback, :running_back_one, :running_back_two, :wide_receiver_one, :wide_receiver_two, :tight_end, :kicker, :defense, :wins, :losses, presence: true
 end
